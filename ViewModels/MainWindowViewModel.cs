cd9275fc0ab1bcaa52cf8aa34a206b47929f9c23
Alex Huynh <alex.va.huynh@gmail.com>
2025-01-30 10:12:12 -0800

    simplified the project. removed all scottplot references and chart-related code, removed history tracking from view model, simplified the ui to focus on current values, kept the color changing funcationality for warnings, and maintained the responsive layout

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Windows.Input;
 using PrinterMonitorDemo.Models;
 using ReactiveUI;
-using System.Linq;
 
 namespace PrinterMonitorDemo.ViewModels
 {
@@ -12,10 +9,6 @@ namespace PrinterMonitorDemo.ViewModels
     {
         private PrinterHead _printerHead;
         private Random _random = new Random();
-        private List<double> _temperatureHistory = new List<double>();
-        private List<double> _inkHistory = new List<double>();
-        private List<(double x, double y)> _positionHistory = new List<(double x, double y)>();
-        private const int HistoryLength = 20;
 
         public PrinterHead PrinterHead
         {
@@ -23,12 +16,13 @@ namespace PrinterMonitorDemo.ViewModels
             set => this.RaiseAndSetIfChanged(ref _printerHead, value);
         }
 
-        public List<double> TemperatureHistory => _temperatureHistory;
-        public List<double> InkHistory => _inkHistory;
-        public List<(double x, double y)> PositionHistory => _positionHistory;
-
         public bool HasTemperatureWarning => PrinterHead.Temperature > 75;
         public bool HasInkWarning => PrinterHead.InkLevel < 20;
+
+        // Color properties
+        public string TemperatureColor => HasTemperatureWarning ? "#ff4444" : "#44ff44";
+        public string InkLevelColor => HasInkWarning ? "#ff4444" : "#4488ff";
+        public string StatusColor => PrinterHead.Status == "Operating Normally" ? "#44ff44" : "#ff4444";
 
         public MainWindowViewModel()
         {
@@ -63,22 +57,12 @@ namespace PrinterMonitorDemo.ViewModels
                 // Update status based on conditions
                 PrinterHead.Status = DetermineStatus();
 
-                // Update history
-                _temperatureHistory.Add(PrinterHead.Temperature);
-                _inkHistory.Add(PrinterHead.InkLevel);
-                _positionHistory.Add((PrinterHead.XPosition, PrinterHead.YPosition));
-
-                // Keep history at fixed length
-                if (_temperatureHistory.Count > HistoryLength)
-                    _temperatureHistory.RemoveAt(0);
-                if (_inkHistory.Count > HistoryLength)
-                    _inkHistory.RemoveAt(0);
-                if (_positionHistory.Count > HistoryLength)
-                    _positionHistory.RemoveAt(0);
-
                 this.RaisePropertyChanged(nameof(PrinterHead));
                 this.RaisePropertyChanged(nameof(HasTemperatureWarning));
                 this.RaisePropertyChanged(nameof(HasInkWarning));
+                this.RaisePropertyChanged(nameof(TemperatureColor));
+                this.RaisePropertyChanged(nameof(InkLevelColor));
+                this.RaisePropertyChanged(nameof(StatusColor));
                 
                 await Task.Delay(1000); // Update every second
             }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,121 +1,21 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using Avalonia.Threading;
 using PrinterMonitorDemo.ViewModels;
-using System;
-using System.Linq;
-using ScottPlot;
-using ScottPlot.Avalonia;
 
 namespace PrinterMonitorDemo.Views
 {
     public partial class MainWindow : Window
     {
-        private readonly DispatcherTimer _timer;
-        private readonly MainWindowViewModel _viewModel;
-
         public MainWindow()
         {
             InitializeComponent();
-            _viewModel = new MainWindowViewModel();
-            DataContext = _viewModel;
-
-            InitializePlots();
-
-            _timer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromSeconds(1)
-            };
-            _timer.Tick += Timer_Tick;
-            _timer.Start();
+            DataContext = new MainWindowViewModel();
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
-        }
-
-        private void InitializePlots()
-        {
-            // Configure Temperature Plot
-            var tempPlot = this.FindControl<AvaPlot>("TemperaturePlot");
-            if (tempPlot != null)
-            {
-                tempPlot.Plot.Title("Temperature History");
-                tempPlot.Plot.XLabel("Time (s)");
-                tempPlot.Plot.YLabel("Temperature (°C)");
-                tempPlot.Plot.SetAxisLimits(yMin: 45, yMax: 85);
-                tempPlot.Plot.Style(figureBackground: System.Drawing.Color.Transparent,
-                                  dataBackground: System.Drawing.Color.Transparent);
-            }
-
-            // Configure Position Plot
-            var posPlot = this.FindControl<AvaPlot>("PositionPlot");
-            if (posPlot != null)
-            {
-                posPlot.Plot.Title("Position History");
-                posPlot.Plot.XLabel("X Position");
-                posPlot.Plot.YLabel("Y Position");
-                posPlot.Plot.SetAxisLimits(0, 100, 0, 100);
-                posPlot.Plot.Style(figureBackground: System.Drawing.Color.Transparent,
-                                 dataBackground: System.Drawing.Color.Transparent);
-            }
-
-            // Configure Ink Level Plot
-            var inkPlot = this.FindControl<AvaPlot>("InkPlot");
-            if (inkPlot != null)
-            {
-                inkPlot.Plot.Title("Ink Level History");
-                inkPlot.Plot.XLabel("Time (s)");
-                inkPlot.Plot.YLabel("Ink Level (%)");
-                inkPlot.Plot.SetAxisLimits(yMin: 0, yMax: 100);
-                inkPlot.Plot.Style(figureBackground: System.Drawing.Color.Transparent,
-                                 dataBackground: System.Drawing.Color.Transparent);
-            }
-        }
-
-        private void Timer_Tick(object? sender, EventArgs e)
-        {
-            UpdatePlots();
-        }
-
-        private void UpdatePlots()
-        {
-            // Update Temperature Plot
-            var tempPlot = this.FindControl<AvaPlot>("TemperaturePlot");
-            if (tempPlot != null)
-            {
-                var tempData = _viewModel.TemperatureHistory.ToArray();
-                var tempXs = Enumerable.Range(0, tempData.Length).Select(i => (double)i).ToArray();
-                tempPlot.Plot.Clear();
-                tempPlot.Plot.AddScatter(tempXs, tempData, color: System.Drawing.Color.LightGreen);
-                tempPlot.Refresh();
-            }
-
-            // Update Position Plot
-            var posPlot = this.FindControl<AvaPlot>("PositionPlot");
-            if (posPlot != null)
-            {
-                var posXs = _viewModel.PositionHistory.Select(p => p.x).ToArray();
-                var posYs = _viewModel.PositionHistory.Select(p => p.y).ToArray();
-                posPlot.Plot.Clear();
-                posPlot.Plot.AddScatter(posXs, posYs, color: System.Drawing.Color.LightBlue);
-                posPlot.Plot.AddPoint(_viewModel.PrinterHead.XPosition, _viewModel.PrinterHead.YPosition,
-                                    color: System.Drawing.Color.Red, size: 10);
-                posPlot.Refresh();
-            }
-
-            // Update Ink Level Plot
-            var inkPlot = this.FindControl<AvaPlot>("InkPlot");
-            if (inkPlot != null)
-            {
-                var inkData = _viewModel.InkHistory.ToArray();
-                var inkXs = Enumerable.Range(0, inkData.Length).Select(i => (double)i).ToArray();
-                inkPlot.Plot.Clear();
-                inkPlot.Plot.AddScatter(inkXs, inkData, color: System.Drawing.Color.LightBlue);
-                inkPlot.Refresh();
-            }
         }
     }
 }
